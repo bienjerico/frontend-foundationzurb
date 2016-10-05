c2e17a760a35e2ebedc467ea0480b3867e656f03
Benj Cueto <benj.cueto@music-group.com>
2016-10-05 15:57:27 +0800

    first setup

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,31 @@
-$(document).foundation();
+/*
+	Author: Bien Jerico Cueto
+	File name: app.js
+	Description: Initialize the main Javascript functionality of Frontend Foundation Zurb Website
+	Dependencies: jQuery
+*/
+
+var FRONTEND = FRONTEND || {};
+
+(function($){
+
+	FRONTEND.loadPage = {
+		init: function(){
+			console.log("success");
+      this.initSuccess();
+		},
+    initSuccess: function(){
+      $('.row').css('color','green');
+    }
+	}
+
+
+	// this will initialize the functions
+	$(function(){
+		// this will initialize foundation js dependencies
+		$(document).foundation();
+		// this will call the loadPage.init function
+		FRONTEND.loadPage.init();
+	});
+
+})(jQuery);
